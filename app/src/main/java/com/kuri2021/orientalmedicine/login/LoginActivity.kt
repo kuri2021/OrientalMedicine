52f5d99816f837a94dfeda88730e60fb3376533a
kuri2021 <78577063+kuri2021@users.noreply.github.com>
2022-09-26 10:15:00 +0900

    22.09.26 intent test

--- app/src/main/java/com/kuri2021/orientalmedicine/login/LoginActivity.kt
+++ app/src/main/java/com/kuri2021/orientalmedicine/login/LoginActivity.kt
@@ -1,5 +1,6 @@
 package com.kuri2021.orientalmedicine.login
 
+import android.content.Intent
 import android.os.Bundle
 import android.os.Handler
 import android.view.View
@@ -10,6 +11,7 @@ import android.widget.RelativeLayout
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
+import com.kuri2021.orientalmedicine.MainActivity
 import com.kuri2021.orientalmedicine.R
 import com.kuri2021.orientalmedicine.databinding.ActivityLoginBinding
 import com.kuri2021.orientalmedicine.login.viewmodel.LoginViewModel
@@ -19,7 +21,7 @@ class LoginActivity : AppCompatActivity() {
     private lateinit var mBinding: ActivityLoginBinding
     private val model: LoginViewModel by viewModels()
     private lateinit var login_layout: LinearLayout
-    private lateinit var login_gradation:View
+    private lateinit var login_gradation: View
 
     private lateinit var fadeInAnim: Animation
 
@@ -32,7 +34,7 @@ class LoginActivity : AppCompatActivity() {
         mBinding.loginviewmodel = model
 
         login_layout = findViewById(R.id.login_layout)
-        login_gradation= findViewById(R.id.login_gradation)
+        login_gradation = findViewById(R.id.login_gradation)
         fadeInAnim()
 
     }
@@ -41,9 +43,12 @@ class LoginActivity : AppCompatActivity() {
         fadeInAnim = AnimationUtils.loadAnimation(this, R.anim.fade)
         login_gradation.startAnimation(fadeInAnim)
         Handler().postDelayed(Runnable {
-            login_layout.visibility=View.VISIBLE
+            login_layout.visibility = View.VISIBLE
         }, 700) // 0.6초 정도 딜레이를 준 후 시작
+    }
 
-
+    private fun initObserve() {
+        val intent = Intent(this, MainActivity::class.java)
+        startActivity(intent)
     }
 }
--- app/src/main/java/com/kuri2021/orientalmedicine/login/viewmodel/LoginViewModel.kt
+++ app/src/main/java/com/kuri2021/orientalmedicine/login/viewmodel/LoginViewModel.kt
@@ -1,32 +1,32 @@
 package com.kuri2021.orientalmedicine.login.viewmodel
 
+import android.app.Activity
 import android.app.Application
+import android.content.Intent
 import android.util.Log
 import android.view.animation.Animation
 import android.view.animation.AnimationUtils
-import androidx.lifecycle.AndroidViewModel
-import androidx.lifecycle.MutableLiveData
-import com.kuri2021.orientalmedicine.R
+import androidx.lifecycle.*
 
-class LoginViewModel(application: Application):AndroidViewModel(application) {
+class LoginViewModel(application: Application) : AndroidViewModel(application) {
 
-    private val context=getApplication<Application>().applicationContext
-    var imag=MutableLiveData<Int>()
+    val sampleText: MutableLiveData<String> = MutableLiveData()
+
+    private val context = getApplication<Application>().applicationContext
+    var imag = MutableLiveData<Int>()
     //    애니매이션(in)
 
-    val login_title="LOGIN"
-    val login_id_edit_title="ID"
-    val login_pw_edit_title="PASSWORD"
+    val login_title = "LOGIN"
+    val login_id_edit_title = "ID"
+    val login_pw_edit_title = "PASSWORD"
 
 
     init {
-        imag.value=0
+        imag.value = 0
     }
 
 
-    fun login(){
-        Log.d("login","로그인 진행")
+    fun login() {
+        Log.d("login", "로그인 진행")
     }
-
-
 }
